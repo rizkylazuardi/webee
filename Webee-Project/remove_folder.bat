9c8db44da4c9a237466423b6ad031e1d8ffef4db
rizkylazuardi <rizky.lazuardi1@gmail.com>
2017-02-22 13:49:30 +0700

    commit sementara

--- Webee-Project/remove_folder.bat
+++ Webee-Project/remove_folder.bat
@@ -1,32 +1,8 @@
-del webee-businesslogic\.classpath
-del webee-businesslogic\.project
-rmdir /S /Q webee-businesslogic\.settings
-rmdir /S /Q webee-businesslogic\target
-rmdir /S /Q webee-businesslogic\bin
-
 del webee-core\.classpath
 del webee-core\.project
 rmdir /S /Q webee-core\.settings
 rmdir /S /Q webee-core\target
 rmdir /S /Q webee-core\bin
-
-del webee-integration\.classpath
-del webee-integration\.project
-rmdir /S /Q webee-integration\.settings
-rmdir /S /Q webee-integration\target
-rmdir /S /Q webee-integration\bin
-
-del webee-persistence\.classpath
-del webee-persistence\.project
-rmdir /S /Q webee-persistence\.settings
-rmdir /S /Q webee-persistence\target
-rmdir /S /Q webee-persistence\bin
-
-del webee-service\.classpath
-del webee-service\.project
-rmdir /S /Q webee-service\.settings
-rmdir /S /Q webee-service\target
-rmdir /S /Q webee-service\bin
 
 del webee-web\.classpath
 del webee-web\.project
---
+++ Webee-Project/webee-ws/src/main/java/com/anabatic/webee/ws/api/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.anabatic.webee.ws.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anabatic.pmo.persistence.entity.EmployeeGroup;
+import com.anabatic.pmo.service.api.EmployeeGroupService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+	@Autowired 
+	private EmployeeGroupService groupService;
+	
+	@RequestMapping(value = "getAllGroup", method = RequestMethod.POST, consumes = "application/json")
+	ResponseEntity<List<EmployeeGroup>> getAllEmployeeGroup() {
+		List<EmployeeGroup> response = groupService.findAll(new EmployeeGroup());
+		
+		return new ResponseEntity<List<EmployeeGroup>>(response, HttpStatus.OK);
+	}
+}
